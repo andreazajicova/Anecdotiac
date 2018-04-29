8d24ab009f596159418de2c5a57431b653f55093
Andrea Zajicova <andre.oneseventwo@gmail.com>
2018-04-29 11:09:33 +0000

    Add category page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @anecdote = Anecdote.all
+    
+    def vegetables
+      @anecdote = Anecdote.all.where(category: "vegetable")
+    end
   end
 end
